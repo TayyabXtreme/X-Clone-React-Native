41fbc8d39cde1809d64674584d46a8aa394de4c1
Muhammad Tayyab <tayyabxtrem@gmail.com>
2025-07-19 13:30:50 +0500

    user routes added

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,52 +1,63 @@
 import mongoose from "mongoose";
 
-const userSchema=new mongoose.Schema({
-    clerkId:{
-        type:String,
-        required:true,
-        unique:true
+const userSchema = new mongoose.Schema(
+  {
+    clerkId: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    firstName:{
-        type:String,
-        required:true
+    firstName: {
+      type: String,
+      required: true,
     },
-    username:{
-        type:String,
-        required:true,
-        unique:true
+    lastName: {
+      type: String,
+      required: true,
     },
-    profilePicture:{
-        type:String,
-        default:""
+    username: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    bannerImage:{
-        type:String,
-        default:""
+    profilePicture: {
+      type: String,
+      default: "",
     },
-    bio:{
-        type:String,
-        default:"",
-        maxLength:160
+    bannerImage: {
+      type: String,
+      default: "",
     },
-    followers:[
-        {
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"User"
-        }
+    bio: {
+      type: String,
+      default: "",
+      maxLength: 160,
+    },
+    location: {
+      type: String,
+      default: "",
+    },
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
     ],
-    following:[
-        {
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"User"
-        }
-    ]
-},{timestamps:true})
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
+const User = mongoose.model("User", userSchema);
 
-const User=mongoose.model("User",userSchema)
 export default User;
