45dd5f096293232a9f5b34834024bf6724da4edb
Muhammad Tayyab <tayyabxtrem@gmail.com>
2025-07-20 23:18:53 +0500

    basic of react native ,Authentication setup and Tab navigation setup

--- mobile/app/(tabs)/search.tsx
+++ mobile/app/(tabs)/search.tsx
@@ -1,15 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import SignOutButton from '@/components/SignOutButton'
 
-const HomeScreen = () => {
+const SearchScreen = () => {
   return (
     <SafeAreaView>
-      <Text>HomeScreen</Text>
-      <SignOutButton/>
+      <Text>SearchScreen</Text>
     </SafeAreaView>
   )
 }
 
-export default HomeScreen
+export default SearchScreen
